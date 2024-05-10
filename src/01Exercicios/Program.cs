f8e7c02ceef47d87ceb0f353efd5da457a10c66a
PauloAlves8039 <pj38alves@gmail.com>
2024-05-10 20:30:53 -0300

    feat: resolvendo exercício 01

--- src/01Exercicios/Program.cs
+++ src/01Exercicios/Program.cs
@@ -1,8 +1,21 @@
-﻿internal class Program
+﻿using _01Exercicios;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("********** Exercícios Delegate **********");
+
+        #region Exercício 01
+
+        var listaDePessoas = Pessoa.ObterListaDePessoas();
+
+        Console.WriteLine("\nExercício 01 - Imprimindo o Nome e Idade de cada pessoa.");
+
+        Action<Pessoa> imprimirListaDePessoas = p => Console.WriteLine($"{p.Nome} - {p.Idade}");
+        listaDePessoas.ForEach(imprimirListaDePessoas);
+
+        #endregion
 
         Console.ReadKey();
     }
